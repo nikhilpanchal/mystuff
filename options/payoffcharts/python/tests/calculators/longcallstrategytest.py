60c22335c1110c9f3c76a057f1f6cba52a7c9b57
nikhilpanchal <nikhilpanchal@gmail.com>
2017-04-13 21:52:51 -0400

    Added test cases for the strategy classes.

--- options/payoffcharts/python/tests/calculators/longcallstrategytest.py
+++ options/payoffcharts/python/tests/calculators/longcallstrategytest.py
@@ -8,7 +8,6 @@ class LongCallStrategyTest(unittest.TestCase):
     STRATEGY_NAME = "Buy_Stock"
 
     def setUp(self):
-        super().setUp()
         self.long_call = LongCallStrategy()
 
     def test_calculate_payoff(self):
@@ -18,5 +17,12 @@ class LongCallStrategyTest(unittest.TestCase):
         pay_off = self.long_call.calculate_payoff(leg, 150)
 
         self.assertEqual(pay_off, 45, "The payoff will be the difference between the stock price and the strike")
+
+    def test_out_of_the_money_call(self):
+        leg = OptionLeg("Buy", "Call", 100, 5.0)
+
+        pay_off = self.long_call.calculate_payoff(leg, 90)
+
+        self.assertEqual(pay_off, -5.0, "The payoff will be the loss on the price paid with no upside")
 
 
